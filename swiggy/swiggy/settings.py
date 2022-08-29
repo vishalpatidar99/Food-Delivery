4d9353316c861c3b46c3d3d5888d03101ef100a3
Vishal Patidar <vishupatidar10@gmail.com>
2022-08-29 13:25:52 +0530

    custom 404

--- swiggy/swiggy/settings.py
+++ swiggy/swiggy/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o51_dwm*wpjt!%^o=cyq*opwf@dd7@cirju8n4w=sjkl#b8zo_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ["*"]
 
